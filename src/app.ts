000c4a15c3cafd8e732ce0171fc13ad856217fb8
iamyuviii <bt22csh025@iiit.ac.in>
2025-05-27 13:23:41 +0530

    routesupdate

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 import { errorHandler } from './middlewares/error.middleware';
 import authRoutes from './routes/auth.routes';
 import propertyRoutes from './routes/property.routes';
-import favoriteRoutes from './routes/fav.routes';
+import favoriteRoutes from './routes/favorite.routes';
 import recommendationRoutes from './routes/recommendation.routes';
 
 dotenv.config();
@@ -14,6 +14,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Root route
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Property Listing API is running',
+    endpoints: {
+      auth: '/api/auth',
+      properties: '/api/properties',
+      favorites: '/api/favorites',
+      recommendations: '/api/recommendations'
+    }
+  });
+});
 
 // Routes
 app.use('/api/auth', authRoutes);
